146c1676e14d5f53f4e1e89f26b82c71cae52ce3
Michael Gurov <mikhail.gurov@workato.com>
2025-05-21 10:23:44 +0200

    replier to return http code + error

--- httpreply/common.go
+++ httpreply/common.go
@@ -10,13 +10,13 @@ import (
 
 // Replier defines some common and useful functions
 type Replier interface {
-	ReplyOK(w http.ResponseWriter)                                   // HTTP 200 { "result": "ok" }
-	ReplyCreated(w http.ResponseWriter)                              // HTTP 201
-	ReplyNoContent(w http.ResponseWriter)                            // HTTP 204
-	ReplyError(w http.ResponseWriter, err error, code int)           // HTTP 4xx { "error": "<...>" }
-	ReplyJSON(w http.ResponseWriter, data interface{})               // HTTP 200 <serialized object>
-	ReplyJSONCode(w http.ResponseWriter, data interface{}, code int) // HTTP xxx <serialized object>
-	ReplyString(w http.ResponseWriter, str string)                   // HTTP 200 <text>
+	ReplyOK(w http.ResponseWriter) (int, error)                                   // HTTP 200 { "result": "ok" }
+	ReplyCreated(w http.ResponseWriter) (int, error)                              // HTTP 201
+	ReplyNoContent(w http.ResponseWriter) (int, error)                            // HTTP 204
+	ReplyError(w http.ResponseWriter, err error, code int) (int, error)           // HTTP 4xx { "error": "<...>" }
+	ReplyJSON(w http.ResponseWriter, data interface{}) (int, error)               // HTTP 200 <serialized object>
+	ReplyJSONCode(w http.ResponseWriter, data interface{}, code int) (int, error) // HTTP xxx <serialized object>
+	ReplyString(w http.ResponseWriter, str string) (int, error)                   // HTTP 200 <text>
 }
 
 type replier struct {
@@ -27,67 +27,60 @@ func NewReplier(logger log.MetaLogger) Replier {
 	return &replier{logger: logger}
 }
 
-func (r *replier) ReplyOK(w http.ResponseWriter) {
+func (r *replier) ReplyOK(w http.ResponseWriter) (int, error) {
 	result := `{ "result": "ok" }`
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Content-Length", strconv.Itoa(len(result)))
 	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write([]byte(result)); err != nil {
-		r.logger.Error("replyOK", log.Error(err))
-	}
+	_, err := w.Write([]byte(result))
+	return http.StatusOK, err
 }
 
-func (r *replier) ReplyCreated(w http.ResponseWriter) {
+func (r *replier) ReplyCreated(w http.ResponseWriter) (int, error) {
 	w.WriteHeader(http.StatusCreated)
-	if _, err := w.Write(nil); err != nil {
-		r.logger.Error("replyCreated", log.Error(err))
-	}
+	_, err := w.Write(nil)
+	return http.StatusCreated, err
 }
 
-func (r *replier) ReplyNoContent(w http.ResponseWriter) {
+func (r *replier) ReplyNoContent(w http.ResponseWriter) (int, error) {
 	w.WriteHeader(http.StatusNoContent)
-	if _, err := w.Write(nil); err != nil {
-		r.logger.Error("replyNoContent", log.Error(err))
-	}
+	_, err := w.Write(nil)
+	return http.StatusNoContent, err
 }
 
-func (r *replier) ReplyError(w http.ResponseWriter, err error, code int) {
+func (r *replier) ReplyError(w http.ResponseWriter, err error, code int) (int, error) {
 	res := map[string]string{"error": err.Error()}
 	result, err := json.Marshal(res)
 	if err != nil {
-		r.logger.Error("replyError", log.Error(err))
+		return http.StatusInternalServerError, err
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Content-Length", strconv.Itoa(len(result)))
 	w.WriteHeader(code)
 	_, err = w.Write(result)
-	if err != nil {
-		r.logger.Error("replyError", log.Error(err))
-	}
+	return code, err
 }
 
-func (r *replier) ReplyJSONCode(w http.ResponseWriter, data interface{}, code int) {
+func (r *replier) ReplyJSONCode(w http.ResponseWriter, data interface{}, code int) (int, error) {
 	result, err := json.Marshal(data)
 	if err != nil {
-		r.logger.Error("replyJSON", log.Error(err))
+		return http.StatusInternalServerError, err
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Content-Length", strconv.Itoa(len(result)))
 	w.WriteHeader(code)
-	if _, err := w.Write(result); err != nil {
-		r.logger.Error("replyJSON", log.Error(err))
-	}
+	_, err = w.Write(result)
+	return http.StatusOK, err
 }
 
-func (r *replier) ReplyJSON(w http.ResponseWriter, data interface{}) {
-	r.ReplyJSONCode(w, data, http.StatusOK)
+func (r *replier) ReplyJSON(w http.ResponseWriter, data interface{}) (int, error) {
+	return r.ReplyJSONCode(w, data, http.StatusOK)
 }
 
-func (r *replier) ReplyString(w http.ResponseWriter, str string) {
+func (r *replier) ReplyString(w http.ResponseWriter, str string) (int, error) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.Header().Set("Content-Length", strconv.Itoa(len(str)))
 	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write([]byte(str)); err != nil {
-		r.logger.Error("replyString", log.Error(err))
-	}
+	_, err := w.Write([]byte(str))
+	return http.StatusOK, err
 }
