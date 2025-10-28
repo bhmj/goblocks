ee134f5704042edfb6b670dba55c45ea9830a4e8
Michael Gurov <i.gourov@gmail.com>
2025-10-28 11:52:30 +0100

    remove default for http group

--- app/config.go
+++ app/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Config struct {
-	HTTP          httpserver.Config `yaml:"http" group:"HTTP endpoint configuration" default:"8080"`
+	HTTP          httpserver.Config `yaml:"http" group:"HTTP endpoint configuration"`
 	Sentry        sentry.Config     `yaml:"sentry" group:"Sentry configuration"`
 	ShutdownDelay time.Duration     `yaml:"shutdown_delay" description:"Time to wait before shutting down"`
 	LogLevel      string            `yaml:"log_level" description:"Log level in production mode" default:"info" choices:"debug,info,warn,error,dpanic,panic,fatal"`
